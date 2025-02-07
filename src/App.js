7bcf922e6a13f0c20701b94cf3438d2c8daa27e3
KrishaNaldo <naldokrisha1@gmail.com>
2025-02-07 12:40:09 +0800

    drake fix the navigations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ApplicationForm from './Pages/ApplicationForm/ApplicationForm';
 import ExistingApplication from './Pages/ExistingApplication/ExistingApplication';
 import TrackingDocument from './Pages/TrackingDocument/TrackingDocument';
 import Checklist from './Pages/Checklist/Checklist';
+import ExamForm from './Pages/Eng-Ins-Exam/Eng-Ins Exam'
+import MatchingExercise from './Pages/ExamSection2/ExamSection2'
 
 function App() {
     return (
@@ -19,6 +21,8 @@ function App() {
                 <Route path="/safety/:role/existing" element={<ExistingApplication />} />
                 <Route path="/safety/:role/existing/:trackingcode" element={<TrackingDocument />} />
                 <Route path="/safety/:role/existing/:trackingcode/checklist" element={<Checklist />} />
+                <Route path="/safety/:role/existing/exam" element={<ExamForm />} />
+                <Route path="/safety/:role/existing/exam/matching" element={<MatchingExercise />} />
             </Routes>
         </Router>
     );
