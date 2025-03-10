37d20acc7017f5bc33c2e9b202f38834dd00e45c
KrishaNaldo <naldokrisha1@gmail.com>
2025-03-10 15:56:25 +0800

    wowowow

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import SafetyRole from './Pages/SafetyRole/SafetyRole';
 import ApplicationForm from './Pages/ApplicationForm/ApplicationForm';
 import ExistingApplication from './Pages/ExistingApplication/ExistingApplication';
 import TrackingDocument from './Pages/TrackingDocument/TrackingDocument';
+import ExamList from './Pages/ExamList/Exam'; 
 import Checklist from './Pages/Checklist/Checklist';
 import ExamForm from './Pages/Eng-Ins-Exam/Eng-Ins Exam'
 import MatchingExercise from './Pages/ExamSection2/ExamSection2'
+import LoginForm from './Pages/Login/Login2';
+import AdminDashboard from './Pages/Admin Dashboard/Adboard';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/Admin"  element={<AdminDashboard />} />
                 <Route path="/safety/:role" element={<SafetyRole />} />
                 <Route path="/safety/:role/new" element={<ApplicationForm />} /> {/* Updated route */}
                 <Route path="/safety/:role/existing" element={<ExistingApplication />} />
                 <Route path="/safety/:role/existing/:trackingcode" element={<TrackingDocument />} />
                 <Route path="/safety/:role/existing/:trackingcode/checklist" element={<Checklist />} />
+                <Route path="/safety/:role/existing/exams" element={<ExamList />} />
                 <Route path="/safety/:role/existing/exam" element={<ExamForm />} />
                 <Route path="/safety/:role/existing/exam/matching" element={<MatchingExercise />} />
+                
             </Routes>
         </Router>
     );
